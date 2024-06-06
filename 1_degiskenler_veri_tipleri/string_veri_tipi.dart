8ccab12d7efa99a9b79b0aeb49d477fb7e447399
Sami GÜL <148059353+samiigll@users.noreply.github.com>
2024-06-06 17:50:58 +0300

    feat: Add triangle perimeter calculation and personal information display

--- 1_degiskenler_veri_tipleri/string_veri_tipi.dart
+++ 1_degiskenler_veri_tipleri/string_veri_tipi.dart
@@ -16,4 +16,23 @@ void main(List<String> args) {
   print(tamAd.length); // length metodu
   print(
       "Adınız $ad ve soyadınız $soyad ve bu ikisinin toplam karakter sayısı: ${tamAd.length} dir.");
+
+  int birinciKenar = 5;
+  int ikinciKenar = 6;
+  int ucuncuKenar = 7;
+  int ucgenCevresi = birinciKenar + ikinciKenar + ucuncuKenar;
+  print("Birinci kenar " +
+      birinciKenar.toString() +
+      " ikinci kenar " +
+      ikinciKenar.toString() +
+      " üçüncü kenar " +
+      ucuncuKenar.toString() +
+      " olan üçgenin çevresi: " +
+      ucgenCevresi.toString() +
+      " dir.");
+
+  String name = "Sami";
+  int born = 2003;
+  int year = 2024;
+  print("Adım $name ve doğum yılım $born. Yaşım ${year - born}.");
 }
