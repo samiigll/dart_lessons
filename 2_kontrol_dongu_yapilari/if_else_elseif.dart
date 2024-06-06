ef5d2642dca1e96e656cdb6aa570160687a432a9
Sami GÜL <148059353+samiigll@users.noreply.github.com>
2024-06-06 18:21:10 +0300

    Remove unnecessary code files for setup and practice

---
+++ 2_kontrol_dongu_yapilari/if_else_elseif.dart
@@ -0,0 +1,11 @@
+void main(List<String> args) {
+  int sayi1 = 6, sayi2 = 6;
+
+  if (sayi1 > sayi2) {
+    print("$sayi1 sayısı $sayi2 sayısından büyüktür.");
+  } else if (sayi1 < sayi2) {
+    print("$sayi1 sayısı $sayi2 sayısından küçüktür.");
+  } else {
+    print("Sayılar eşittir.");
+  }
+}
