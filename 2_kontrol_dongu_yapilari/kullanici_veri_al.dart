eee7b05c1728e310bdd8e0f6f9ff74243c26297d
Sami GÜL <148059353+samiigll@users.noreply.github.com>
2024-06-09 20:30:06 +0300

    chore: Refactor kullanici_veri_al.dart for improved user input handling and added functionality

--- 2_kontrol_dongu_yapilari/kullanici_veri_al.dart
+++ 2_kontrol_dongu_yapilari/kullanici_veri_al.dart
@@ -1,27 +1,34 @@
-// import 'dart:io';
-
-// void main(List<String> args) {
-//   print("Adınızı giriniz");
-//   var ad = stdin.readLineSync();
-
-//   print("yaşınızı giriniz:");
-//   var yas = int.parse(stdin.readLineSync()!);
-//   print("girdiği adınız: $ad ve yaşınız: $yas");
-// }
 import 'dart:io';
 
 void main(List<String> args) {
-  // Kullanıcıdan birinci sayıyı iste
+  // İlk kısım: Ad ve yaş alma
+  print("Adınızı giriniz:");
+  var ad = stdin.readLineSync();
+
+  print("Yaşınızı giriniz:");
+  var yas = int.parse(stdin.readLineSync()!);
+  print("Girdiğiniz ad: $ad ve yaş: $yas");
+
+  // İkinci kısım: İki sayının toplamını hesaplama
   print("Birinci sayıyı giriniz:");
   var sayi1 = int.parse(stdin.readLineSync()!);
 
-  // Kullanıcıdan ikinci sayıyı iste
   print("İkinci sayıyı giriniz:");
   var sayi2 = int.parse(stdin.readLineSync()!);
 
-  // İki sayının toplamını hesapla
   var toplam = sayi1 + sayi2;
-
-  // Toplamı ekrana yazdır
   print("Girilen sayıların toplamı: $toplam");
+
+  // Üçüncü kısım: Üç sayının toplamının yarısını hesaplama
+  print("Birinci sayıyı giriniz:");
+  var sayi3 = int.parse(stdin.readLineSync()!);
+
+  print("İkinci sayıyı giriniz:");
+  var sayi4 = int.parse(stdin.readLineSync()!);
+
+  print("Üçüncü sayıyı giriniz:");
+  var sayi5 = int.parse(stdin.readLineSync()!);
+
+  var toplamYarisi = (sayi3 + sayi4 + sayi5) / 2;
+  print("Girilen sayıların toplamının yarısı: $toplamYarisi");
 }
