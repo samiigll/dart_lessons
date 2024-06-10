0f64da90ada775b82d3452427e94c426f84963cd
Sami GÜL <148059353+samiigll@users.noreply.github.com>
2024-06-10 21:48:36 +0300

    refactor: Update parametre_turleri.dart for named parameter usage

--- fonksiyonlar/parametre_turleri.dart
+++ fonksiyonlar/parametre_turleri.dart
@@ -1,7 +1,13 @@
 void main(List<String> args) {
-  int toplam = sayilariTopla(5, 8, 15);
-  int toplam2 = sayilariTopla(10, 15, 20);
-  print("Toplam : $toplam2");
+  /* int toplam = sayilariTopla(5, 8, 15);
+  int toplam2 = sayilariTopla(10, 15, 20); */
+
+  int toplam = sayilariTopla(
+    sayi3: 15,
+    sayi1: 5,
+    sayi2: 8,
+  );
+  print("Toplam : $toplam");
 }
 
 //required parametre
@@ -10,6 +16,11 @@ void main(List<String> args) {
 } */
 
 //optional parametre
-int sayilariTopla(int s1, int s2, [int s3 = 0]) {
+/* int sayilariTopla(int s1, int s2, [int s3 = 0]) {
   return s1 + s2 + s3;
+} */
+
+//named parametre
+int sayilariTopla({int sayi1 = 0, int sayi2 = 0, int sayi3 = 0}) {
+  return sayi1 + sayi2 + sayi3;
 }
