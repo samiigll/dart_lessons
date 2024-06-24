acca9134ee146eb39d056ccfd981143c75229c92
Sami GÜL <148059353+samiigll@users.noreply.github.com>
2024-06-24 20:39:03 +0300

    refactor: Update kurucu_contructor.dart to use constructor for car class

--- 6_nyp_opp_kavramlari/kurucu_contructor.dart
+++ 6_nyp_opp_kavramlari/kurucu_contructor.dart
@@ -1,36 +1,42 @@
-//kurucu metotlar - constructor
 void main(List<String> args) {
+  // Araba sınıfından honda adında bir nesne oluşturuluyor ve kurucu metot çağrılıyor
   Araba honda = Araba("Honda", 2021);
-  /* honda.marka = "Honda";
-  honda.modelYili = 2021; */
-  honda.bilgileriYazdir();
+  honda.bilgileriYazdir(); // Oluşturulan nesnenin bilgileri yazdırılıyor
 
+  // Araba sınıfından toyota adında bir nesne oluşturuluyor ve kurucu metot çağrılıyor
   Araba toyota = Araba("Toyota", 2022);
-  toyota.bilgileriYazdir();
+  toyota.bilgileriYazdir(); // Oluşturulan nesnenin bilgileri yazdırılıyor
 }
 
 class Araba {
-  int? modelYili;
-  String? marka;
+  int? modelYili; // Arabanın model yılı özelliği
+  String? marka; // Arabanın marka özelliği
 
+  // Araba sınıfının kurucu metodu
   Araba(this.marka, this.modelYili) {
+    // Kurucu metot çalıştığında yapılacak işlemler burada tanımlanır
     print("Kurucu metot çalıştı");
+    // Eğer model yılı 2002'den küçükse, 2002 olarak ayarla
     if (this.modelYili! < 2002) {
       this.modelYili = 2002;
     }
   }
 
+  // Araba sınıfının başka bir kurucu metot tanımı
   /* Araba(String marka, int modelYili) {
     print("Kurucu metot çalıştı");
     this.marka = marka;
-    this.modelYili = modelYili */
+    this.modelYili = modelYili;
+  } */
 
-/*   Araba(String m, int yil) {
+  // Araba sınıfının bir başka kurucu metot tanımı
+  /* Araba(String m, int yil) {
     print("Kurucu metot çalıştı");
     marka = m;
     modelYili = yil;
   } */
 
+  // Araba nesnesinin bilgilerini yazdıran fonksiyon
   void bilgileriYazdir() {
     print("Araba markası: $marka, model yılı: $modelYili");
   }
