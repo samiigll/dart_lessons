b7846fdda4dea288c62e6fe2487ec9ddd9c333bf
Sami GÜL <148059353+samiigll@users.noreply.github.com>
2024-06-29 15:33:03 +0300

    nul safety part 1

---
+++ 7_functional_programming_null_safety/null_safety_bolum1.dart
@@ -0,0 +1,15 @@
+//Nullable, non-nonllable, assertion operator
+
+void main(List<String> args) {
+/*   int? a;
+  a = null;
+  a = 5;
+  a = null;
+  print(a+5); // Error: The operand can't be null, so the operator '[]' is not allowed. */
+
+/* List<String> isimler = ["Ali", "Veli", "Selami"];
+List<String> renkler = null;
+print(renkler);
+List<String>? markalar = ["Audi", "BMW", "Mercedes"];
+List<String?>? test; */
+}
